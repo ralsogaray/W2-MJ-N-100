00bf74c7e0aba19057f9fb1f5d497d852e918d63
smessina <info@silviomessina.com>
2020-10-06 20:43:22 -0300

    Configurando archivos estaticos

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express')
 
 const server = express()
 
+const docs = express.static("docs")
+
+server.use( docs )
+
 server.listen(2000)
 
-server.get("/", function(request, response){
+server.post("/", function(request, response){
     //ACA HAY QUE PROGRAMAR LA RESPUESTA MAGICA... Woooo
     response.write("<h1>Hola clase desde Node.js!</h1>")
     response.write("<h2>Con demonios incluidos...</h2>")
