f41c64b8c0b80574acb8777204a0e615b0201319
smessina <info@silviomessina.com>
2020-10-06 21:37:32 -0300

    Implementando configs para procesamiento POST

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ const express = require('express')
 const server = express()
 
 const docs = express.static("docs")
+const urlencoded = express.urlencoded({ extended : true }) // <-- Convierte de FormData a Object
+const json = express.json() // <-- Convierte de JSON a Object
 
 server.use( docs )
+server.use( urlencoded )
+server.use( json )
 
 server.listen(2000)
 
-server.post("/", function(request, response){
-    //ACA HAY QUE PROGRAMAR LA RESPUESTA MAGICA... Woooo
-    response.write("<h1>Hola clase desde Node.js!</h1>")
-    response.write("<h2>Con demonios incluidos...</h2>")
-    response.end("<p>Estamos viendo cosas 'server-side'... woooo</p>")
+server.post("/procesar", function(request, response){
+    console.log(request.body.nombre)
+    response.end("Mira la consola...")
 })
