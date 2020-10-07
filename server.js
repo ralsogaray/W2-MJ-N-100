66343641ebcf4fb3689aba5245a599f9bc01a301
smessina <info@silviomessina.com>
2020-10-06 22:02:43 -0300

    Prototipo de 'Base de Datos'

--- server.js
+++ server.js
@@ -6,13 +6,22 @@ const docs = express.static("docs")
 const urlencoded = express.urlencoded({ extended : true }) // <-- Convierte de FormData a Object
 const json = express.json() // <-- Convierte de JSON a Object
 
+let baseDeProductos = []
+
 server.use( docs )
 server.use( urlencoded )
 server.use( json )
 
 server.listen(2000)
 
-server.post("/procesar", function(request, response){
-    console.log(request.body.nombre)
+server.post("/agregar", function(request, response){
+    /*
+        ACA DEBERIA VALIDAD LOS DATOS CON SUPER MEGA IF Y OTRAS COSAS
+    */
+    baseDeProductos.push( request.body )
+
+    console.log(`La base de datos de productos tiene:`)
+    console.log(baseDeProductos)
+
     response.end("Mira la consola...")
 })
