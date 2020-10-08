b8c34e4e3a264b4842e39cf8b843ff2830d0af24
smessina <info@silviomessina.com>
2020-10-08 20:49:45 -0300

    Incorporando EasyDB

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const easyDB = require('easydb-io')
 
 const server = express()
 
@@ -6,7 +7,10 @@ const docs = express.static("docs")
 const urlencoded = express.urlencoded({ extended : true }) // <-- Convierte de FormData a Object
 const json = express.json() // <-- Convierte de JSON a Object
 
-const baseDeProductos = []
+const baseDeProductos = easyDB({
+    database: 'a4fa64d4-7de9-4e01-8bfb-a5221eced744',
+    token: 'baf07f7c-f47c-4d6d-b1f1-a19a8b12f3bd'
+})
 
 server.use( docs )
 server.use( urlencoded )
@@ -14,14 +18,11 @@ server.use( json )
 
 server.listen(2000)
 
-server.post("/agregar", function(request, response){
+server.post("/agregar", async function(request, response){
     /*
         ACA DEBERIA VALIDAR LOS DATOS CON SUPER MEGA IF Y OTRAS COSAS
     */
-    baseDeProductos.push( request.body )
-
-    console.log(`La base de datos de productos tiene:`)
-    console.log(baseDeProductos)
+    await baseDeProductos.put('1823y49172t', request.body )
 
     response.end("Mira la consola...")
 })
